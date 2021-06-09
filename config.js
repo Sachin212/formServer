3cae93fe227e9e9dcec21fed6ddfd1a5b7913f73
Sachin Kumar <sachinsingh0400@gmail.com>
2021-06-10 01:49:58 +0530

    Removed

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGODB: 'mongodb+srv://Sachin:aSMtbuFB7ikD1OCI@cluster0.8n3ex.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    SECRET_KEY: 'some very secret key'
-}
